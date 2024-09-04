d0ea0bb3ef992d7eac90ad68742f2cf3cc7557c8
Yurii Danko <urilvv1@gmail.com>
2024-09-04 14:16:32 +0300

    initial setup commit

---
+++ src/main/java/com/urilvv/GymApplicationEpam/GymApplicationEpamApplication.java
@@ -0,0 +1,13 @@
+package com.urilvv.GymApplicationEpam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymApplicationEpamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymApplicationEpamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urilvv/GymApplicationEpam/GymApplicationEpamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urilvv.GymApplicationEpam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymApplicationEpamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
