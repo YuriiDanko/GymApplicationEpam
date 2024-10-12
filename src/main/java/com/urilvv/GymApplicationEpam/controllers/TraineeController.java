71a8d05eff2ef2f91deb7ce12cd21d44f5fd8df8
Yurii Danko <urilvv1@gmail.com>
2024-10-12 21:00:35 +0300

    added documentation for API endpoints using OpenAPI library

--- src/main/java/com/urilvv/GymApplicationEpam/controllers/TraineeController.java
+++ src/main/java/com/urilvv/GymApplicationEpam/controllers/TraineeController.java
@@ -11,6 +11,8 @@ import com.urilvv.GymApplicationEpam.requestResponseModels.traineeRequests.Train
 import com.urilvv.GymApplicationEpam.requestResponseModels.traineeRequests.TraineeTrainingsReq;
 import com.urilvv.GymApplicationEpam.services.TraineeService;
 import com.urilvv.GymApplicationEpam.utils.Mapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -20,13 +22,22 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/trainees", produces = {"application/JSON"})
+@RequestMapping(value = "/trainees")
 @AllArgsConstructor
 public class TraineeController {
 
     private final TraineeService traineeService;
     private final Mapper mapper;
 
+    @Operation(
+            description = "GET endpoint to get a list of all Trainees entities.",
+            responses = {
+                    @ApiResponse(
+                            description = "Successfully returned all Trainees.",
+                            responseCode = "200"
+                    )
+            }
+    )
     @GetMapping
     public ResponseEntity<List<TraineeDTO>> getTrainees() {
         List<TraineeDTO> traineeDTOS = traineeService.getAllTrainees()
@@ -37,6 +48,19 @@ public class TraineeController {
         return new ResponseEntity<>(traineeDTOS, HttpStatus.OK);
     }
 
+    @Operation(
+            description = "POST endpoint to register a new Trainee.",
+            responses = {
+                    @ApiResponse(
+                            description = "User was registered successfully.",
+                            responseCode = "201"
+                    ),
+                    @ApiResponse(
+                            description = "Error with processing an request.",
+                            responseCode = "400"
+                    )
+            }
+    )
     @PostMapping("/register")
     public ResponseEntity<RegisterResponse> registerTrainee(@Valid @RequestBody RegisterTraineeRequest traineeReq) {
         Trainee trainee = traineeService.createTrainee(traineeReq.getFirstName(), traineeReq.getLastName(),
@@ -44,28 +68,80 @@ public class TraineeController {
         return new ResponseEntity<>(new RegisterResponse(trainee.getUserId(), trainee.getUsername(), trainee.getPassword()), HttpStatus.CREATED);
     }
 
+    @Operation(
+            description = "GET endpoint to receive Trainee Profile.",
+            responses = {
+                    @ApiResponse(
+                            description = "Profile received successfully.",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "User was not found.",
+                            responseCode = "404"
+                    )
+            }
+    )
     @GetMapping("/profile/{username}")
     public ResponseEntity<TraineeDTO> getTraineeProfile(@PathVariable String username) {
         Trainee trainee = traineeService.selectTrainee(username).orElseThrow(ExceptionHandlerController.UserNotFoundException::new);
         return new ResponseEntity<>(mapper.mapTrainee(trainee), HttpStatus.OK);
     }
 
+    @Operation(
+            description = "DELETE endpoint to delete Trainee record.",
+            responses = {
+                    @ApiResponse(
+                            description = "Trainee was deleted successfully.",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "User was not found.",
+                            responseCode = "404"
+                    )
+            }
+    )
     @DeleteMapping("/delete/{user_id}")
     public ResponseEntity<Object> deleteTrainee(@PathVariable("user_id") String userId) {
         boolean isDeleted = traineeService.deleteTrainee(userId);
         return new ResponseEntity<>(isDeleted, HttpStatus.OK);
     }
 
+    @Operation(
+            description = "GET endpoint to receive Trainee trainings entities.",
+            responses = {
+                    @ApiResponse(
+                            description = "Training were returned successfully.",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Error processing request.",
+                            responseCode = "400"
+                    )
+            }
+    )
     @GetMapping("/{username}/trainings")
     public ResponseEntity<List<TrainingDTO>> getTraineeTrainings(@PathVariable String username,
                                                                  @Valid @RequestBody TraineeTrainingsReq trainingsRequest) {
         List<TrainingDTO> trainings = traineeService.getTraineeTrainingList(username, trainingsRequest.getFrom(),
-                trainingsRequest.getTo(), trainingsRequest.getTrainerName(), TrainingType.getType(trainingsRequest.getTrainingType()))
+                        trainingsRequest.getTo(), trainingsRequest.getTrainerName(), TrainingType.getType(trainingsRequest.getTrainingType()))
                 .stream().map(mapper::mapTraining).toList();
 
         return new ResponseEntity<>(trainings, HttpStatus.OK);
     }
 
+    @Operation(
+            description = "PUT endpoint to change Trainee password.",
+            responses = {
+                    @ApiResponse(
+                            description = "Password was changed successfully.",
+                            responseCode = "204"
+                    ),
+                    @ApiResponse(
+                            description = "User was not found.",
+                            responseCode = "404"
+                    )
+            }
+    )
     @PutMapping("/{user_id}/change-password")
     public ResponseEntity<String> changeTraineePassword(@PathVariable("user_id") String userId,
                                                         @Valid @RequestBody LoginChangeRequest loginChangeReq) {
@@ -73,12 +149,42 @@ public class TraineeController {
         return ResponseEntity.status(HttpStatus.ACCEPTED).body("Password was changed successfully!");
     }
 
+    @Operation(
+            description = "PATCH endpoint to change active status of Trainee.",
+            responses = {
+                    @ApiResponse(
+                            description = "Active status changed successfully.",
+                            responseCode = "204"
+                    ),
+                    @ApiResponse(
+                            description = "Error processing request.",
+                            responseCode = "400"
+                    ),
+                    @ApiResponse(
+                            description = "User was not found.",
+                            responseCode = "404"
+                    )
+            }
+    )
     @PatchMapping("/{user_id}/change-active-status")
     public ResponseEntity<String> changeActiveStatus(@PathVariable("user_id") String userId) {
         traineeService.changeActiveStatus(userId);
         return ResponseEntity.status(HttpStatus.OK).body("Active status was changed successfully!");
     }
 
+    @Operation(
+            description = "PUT endpoint to update Trainee profile.",
+            responses = {
+                    @ApiResponse(
+                            description = "Trainee profile updated successfully.",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "User was not found.",
+                            responseCode = "404"
+                    )
+            }
+    )
     @PutMapping("/{user_id}/update-profile")
     public ResponseEntity<TraineeDTO> updateTraineeProfile(@PathVariable("user_id") String userId,
                                                            @RequestBody TraineeEditRequest traineeEditReq) {
--- src/main/java/com/urilvv/GymApplicationEpam/controllers/TrainerController.java
+++ src/main/java/com/urilvv/GymApplicationEpam/controllers/TrainerController.java
@@ -10,6 +10,8 @@ import com.urilvv.GymApplicationEpam.requestResponseModels.trainerRequests.Train
 import com.urilvv.GymApplicationEpam.requestResponseModels.trainerRequests.TrainerTrainingsReq;
 import com.urilvv.GymApplicationEpam.services.TrainerService;
 import com.urilvv.GymApplicationEpam.utils.Mapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,13 +21,26 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/trainers", produces = {"application/JSON"})
+@RequestMapping(value = "/trainers")
 @AllArgsConstructor
 public class TrainerController {
 
     private final TrainerService trainerService;
     private final Mapper mapper;
 
+    @Operation(
+            description = "POST endpoint to register a new Trainer.",
+            responses = {
+                    @ApiResponse(
+                            description = "User was registered successfully.",
+                            responseCode = "201"
+                    ),
+                    @ApiResponse(
+                            description = "Error with processing an request.",
+                            responseCode = "400"
+                    )
+            }
+    )
     @PostMapping("/register")
     public ResponseEntity<RegisterResponse> registerTrainer(@Valid @RequestBody RegisterTrainerRequest trainerReq) {
         Trainer trainer = trainerService.createTrainer(trainerReq.getFirstName(), trainerReq.getLastName(),
@@ -33,6 +48,15 @@ public class TrainerController {
         return new ResponseEntity<>(new RegisterResponse(trainer.getUserId(), trainer.getUsername(), trainer.getPassword()), HttpStatus.CREATED);
     }
 
+    @Operation(
+            description = "GET endpoint to get a list of all Trainers entities.",
+            responses = {
+                    @ApiResponse(
+                            description = "Successfully returned all Trainers.",
+                            responseCode = "200"
+                    )
+            }
+    )
     @GetMapping
     public ResponseEntity<List<TrainerDTO>> getAllTrainers() {
         List<TrainerDTO> trainersDTO = trainerService.getAllTrainers()
@@ -43,18 +67,61 @@ public class TrainerController {
         return new ResponseEntity<>(trainersDTO, HttpStatus.OK);
     }
 
+    @Operation(
+            description = "GET endpoint to receive Trainer Profile.",
+            responses = {
+                    @ApiResponse(
+                            description = "Profile received successfully.",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "User was not found.",
+                            responseCode = "404"
+                    )
+            }
+    )
     @GetMapping("/profile/{username}")
     public ResponseEntity<TrainerDTO> getTrainerProfile(@PathVariable String username) {
         Trainer trainer = trainerService.selectTrainer(username).orElseThrow(ExceptionHandlerController.UserNotFoundException::new);
         return new ResponseEntity<>(mapper.mapTrainer(trainer), HttpStatus.OK);
     }
 
+    @Operation(
+            description = "PATCH endpoint to change active status of Trainer.",
+            responses = {
+                    @ApiResponse(
+                            description = "Active status changed successfully.",
+                            responseCode = "204"
+                    ),
+                    @ApiResponse(
+                            description = "Error processing request.",
+                            responseCode = "400"
+                    ),
+                    @ApiResponse(
+                            description = "User was not found.",
+                            responseCode = "404"
+                    )
+            }
+    )
     @PatchMapping("/{user_id}/change-active-status")
     public ResponseEntity<String> changeActiveStatus(@PathVariable("user_id") String userId) {
         trainerService.changeActiveStatus(userId);
         return ResponseEntity.status(HttpStatus.OK).body("Active status was changed successfully!");
     }
 
+    @Operation(
+            description = "PUT endpoint to change Trainer password.",
+            responses = {
+                    @ApiResponse(
+                            description = "Password was changed successfully.",
+                            responseCode = "204"
+                    ),
+                    @ApiResponse(
+                            description = "User was not found.",
+                            responseCode = "404"
+                    )
+            }
+    )
     @PutMapping("/{user_id}/change-password")
     public ResponseEntity<String> changeTraineePassword(@PathVariable("user_id") String userId,
                                                         @Valid @RequestBody LoginChangeRequest loginChangeReq) {
@@ -62,6 +129,19 @@ public class TrainerController {
         return ResponseEntity.status(HttpStatus.ACCEPTED).body("Password was changed successfully!");
     }
 
+    @Operation(
+            description = "PUT endpoint to update Trainer profile.",
+            responses = {
+                    @ApiResponse(
+                            description = "Trainer profile updated successfully.",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "User was not found.",
+                            responseCode = "404"
+                    )
+            }
+    )
     @PutMapping("/{user_id}/update-profile")
     public ResponseEntity<TrainerDTO> updateTrainerProfile(@PathVariable("user_id") String userId,
                                                            @RequestBody TrainerEditRequest trainerEditReq) {
@@ -70,6 +150,19 @@ public class TrainerController {
         return new ResponseEntity<>(trainerDTO, HttpStatus.OK);
     }
 
+    @Operation(
+            description = "GET endpoint to receive Trainer trainings entities.",
+            responses = {
+                    @ApiResponse(
+                            description = "Training were returned successfully.",
+                            responseCode = "200"
+                    ),
+                    @ApiResponse(
+                            description = "Error processing request.",
+                            responseCode = "400"
+                    )
+            }
+    )
     @GetMapping("/{username}/trainings")
     public ResponseEntity<List<TrainingDTO>> getTrainerTrainings(@PathVariable String username,
                                                                  @Valid @RequestBody TrainerTrainingsReq trainerTrainingsReq) {
