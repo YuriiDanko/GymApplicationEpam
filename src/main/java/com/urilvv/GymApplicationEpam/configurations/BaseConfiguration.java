031d57553f07320e3925dfeda773d4bc29345ecc
Yurii Danko <urilvv1@gmail.com>
2024-11-07 16:17:22 +0200

    added needed dependency of spring-security and implemented default configuration of spring-security, added encoding logic into User creation object

--- src/main/java/com/urilvv/GymApplicationEpam/configurations/BaseConfiguration.java
+++ src/main/java/com/urilvv/GymApplicationEpam/configurations/BaseConfiguration.java
@@ -4,12 +4,35 @@ import org.passay.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
+@EnableWebSecurity
 @ComponentScan("com.urilvv.*")
 public class BaseConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/trainees/register").permitAll()
+                        .requestMatchers("/trainers/register").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults())
+                .logout(Customizer.withDefaults());
+
+        return http.build();
+    }
 
     @Bean
     public PasswordValidator getPasswordValidator() {
@@ -20,6 +43,11 @@ public class BaseConfiguration {
                 new CharacterRule(EnglishCharacterData.Digit, 1),
                 new WhitespaceRule()
         );
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
